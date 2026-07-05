85224425c253a1225392bbfc6f262ef1c40f1e63
Hadi <shirmohammadi.b2b@gmail.com>
2026-07-04 20:44:03 -0700

    add User entity and repository with PostgreSQL configuration

---
+++ src/main/java/com/Hospital/backend/Entities/User.java
@@ -0,0 +1,86 @@
+package com.Hospital.backend.Entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstname;
+    private String lastname;
+    private String username;
+    private String email;
+    private String number;
+    private String password;
+    private Boolean isAdmin;
+
+    //getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(Boolean admin) {
+        isAdmin = admin;
+    }
+}
+
---
+++ src/main/java/com/Hospital/backend/Repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.Hospital.backend.Repository;
+
+import com.Hospital.backend.Entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
