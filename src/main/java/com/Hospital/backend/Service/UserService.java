6a93b63b64e62956586dfe9182aea7bd57c10557
Hadi <shirmohammadi.b2b@gmail.com>
2026-07-10 04:21:02 -0700

    feat: integrate spring security and bcrypt for password hashing

--- src/main/java/com/Hospital/backend/Service/UserService.java
+++ src/main/java/com/Hospital/backend/Service/UserService.java
@@ -2,7 +2,9 @@ package com.Hospital.backend.Service;
 
 import com.Hospital.backend.Entities.User;
 import com.Hospital.backend.Repository.UserRepository;
+import org.hibernate.query.sql.internal.ParameterRecognizerImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -11,9 +13,17 @@ import java.util.Optional;
 public class UserService {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     //Register User
     public User registerUser(User user){
+        String hashedPassword =passwordEncoder.encode(user.getPassword());
+        user.setPassword(hashedPassword);
         return userRepository.save(user);
+    }
+    //Chack User password
+    public boolean checkPassword(String rawPassword,String hashedPassword){
+        return passwordEncoder.matches(rawPassword,hashedPassword);
     }
     //Get User by username
     public Optional<User> findByUsername(String username){
