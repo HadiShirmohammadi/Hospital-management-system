2d8f27d9c3f4722668d6999504e8192f469a6826
HadiShirmohammadi <shirmohammadi.b2b@gmail.com>
2026-07-17 07:46:42 -0700

    feat(security): implement JwtService (generateToken, extractUsername, isValidKey)

--- src/main/java/com/Hospital/backend/Service/JwtService.java
+++ src/main/java/com/Hospital/backend/Service/JwtService.java
@@ -1,16 +1,61 @@
 package com.Hospital.backend.Service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
-import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class JwtService {
+    // Create key
     private final Key key;
-    public JwtService(@Value("${JWT_SECRET}") String secretString){
+    public JwtService(@Value("${jwt.secret}") String secretString){
         this.key = Keys.hmacShaKeyFor(secretString.getBytes());
     }
+    //Generate token
+    public String generateToken(String username,boolean isAdmin){
+        Map<String,Object> claims = new HashMap<>();
+        claims.put("isAdmin",isAdmin);
+        long now  = System.currentTimeMillis();
+        long expirationTime = 1000 * 60 * 60 * 12;
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(username)
+                .setIssuedAt(new Date(now))
+                .setExpiration(new Date(now + expirationTime))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+    // Get username from key
+    public String extractUsername(String token){
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claims.getSubject();
+    }
+    // Understanding whether a key is correct or incorrect
+    public boolean isTokenValid(String token){
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e){
+            return false;
+        }
+
+    }
+
 }
